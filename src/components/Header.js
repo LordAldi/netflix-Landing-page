04e0815612a2a9e56f63b831e6a2d39cc546139f
LordAldi <aldianugra09@gmail.com>
2020-06-30 23:26:17 +0700

    jumbutron, call to action button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import logo from '../svg/logo.svg';
-
+import { NavLink, Link } from 'react-router-dom'
+import classes from '../css/Header.module.css'
+import { Icon } from 'react-icons-kit'
+import {ic_keyboard_arrow_right} from 'react-icons-kit/md/ic_keyboard_arrow_right'
 
 
 class Header extends Component {
     render() {
         return (
             <div className='header-container'>
-                <div className='Header-top'>
-                    <img src={logo}/>
+                <div className={classes.headertop}>
+                    <img className={classes.Img} src={logo}/>
+                    <NavLink className={classes.signInbtn} to='/'>Sign In</NavLink>
+                </div>
+
+                <div className={classes.headerContent}>
+                    <h1 className={classes.title}>See what's next.</h1>
+                    <h2 className={classes.subtitle}>WATCH ANYWHERE. CANCEL ANYTIME,</h2>
+                    <Link className={classes.mainOfferBtn}>try it now <Icon icon={ic_keyboard_arrow_right} size={37} className={classes.icon}/></Link>
                 </div>
             </div>
         )
@@ -16,3 +26,4 @@ class Header extends Component {
 }
 
 export default Header
+
